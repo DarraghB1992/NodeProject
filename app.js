51d2a0d1e6907c5812e947bad7c7d807a70bb34d
Darragh <darragh1992@gmail.com>
2016-09-13 10:10:51 +0100

    Added ability to add posts, upvote and add comments

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+mongoose.connect('mongodb://localhost/news');
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,10 +10,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -55,6 +65,8 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
+
+
 
 
 module.exports = app;
